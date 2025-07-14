6926d8afd34de53395f61239808b05781cd158fe
suthakaranburaj <suthakaranburaj@gmail.com>
2025-07-14 23:15:16 +0530

    light mode removed

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import {Hero_section} from "./hero_section/Hero_section";
 import { Navbar } from "./navbar/Navbar";
 import { ServiceSection } from "./Service_section/Service_section";
-import { GlobeDemo } from "./hero_section/Hero_section_left";
 import { PastProjects } from "./projects/PastProjects"
 import { FeedbackSection } from "./feedback/FeedbackSection"
 import { ContactUs } from "./contact_us/ContactUs";
@@ -10,7 +9,6 @@ export default {
   Hero_section,
   Navbar,
   ServiceSection,
-  GlobeDemo,
   PastProjects,
   FeedbackSection,
   ContactUs,
